691d9ca307ada324ff4a2871dcaf10da29aceb0a
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-02-26 21:05:45 +0200

    added individualMovie component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
+import IndividualMovie from './components/IndividualMovie/IndividualMovie';
+import { movieDTO } from './models/movies.model';
 
 function App() {
+  const movie:movieDTO={
+    id:1,
+    title:"אלכס חולה אהבה",
+    poster:"https://pentagon-auctions.com/wp-content/uploads/2021/03/Screenshot_102.png"
+
+  }
   return (
-    <div>
+    <>
+    <IndividualMovie {...movie}/>
      
-    </div>
+    </>
   );
 }
 
